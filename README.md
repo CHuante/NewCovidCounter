48989ed53c2688b1af96e05c519fa27f27c8e566
CHuante <32277113+CHuante@users.noreply.github.com>
2020-12-29 16:34:10 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,44 @@
+# Covid Counter
+
+CPSC-349 Web Front-End Engineering
+Project 2
+
+
+===== TEAM & MEMBERS/TRELLO: =====
+
+Team.js
+
+Richard Stanely
+audstanley@gmail.com
+
+Chase Huante
+cdhuante@csu.fullerton.edu
+
+Collin Campbell 
+collinc1997@gmail.com
+
+Dylan Connors 
+dylanconnors@csu.fullerton.edu
+
+Trello URL for Team.js:
+https://trello.com/b/Ly2XeDrl/task-board
+
+===== DETAILS: =====
+
+This website is a COVID-19 tracker in the United States that can narrow down your search by state and county by collecting data through local databases.  Designed with react and used API's and Iframes to produce charts of positivity, death, and ICU rates. Photoshop was also used for graphic design of favicon and logo.  If there's and issues please feel free to contact us and we'll help out.  
+
+Thank you, and enjoy Covid Counter!
+
+
+===== INSTRUCTIONS: =====
+
+STEP 1: Download folder into computer
+
+STEP 2: Open terminal/command prompt and cd into the project folder
+
+STEP 3: Make sure npm is installed and enter the colloring code:
+
+```bash
+npm run start;
+```
+STEP 4: After that the website should pop-up in a localhost 3000 site. Enjoy!
---
+++ src/App.js
@@ -0,0 +1,498 @@
+import React, { useReducer, useRef, useLayoutEffect, useState, useEffect } from "react";
+import ReactMarkdown from 'react-markdown';
+import clsx from 'clsx';
+import axios from 'axios';
+import usStates from "./usStates";
+import {  AppBar, 
+          Toolbar, 
+          IconButton,
+          Typography } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home';
+import PublicIcon from '@material-ui/icons/Public';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import LocationSearchingIcon from '@material-ui/icons/LocationSearching';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+// for logo
+import image2vector from './image2vector.svg';
+
+
+am4core.useTheme(am4themes_animated);
+am4core.options.autoDispose = true;
+
+const useStyles = makeStyles({
+  root: {
+    border: 0,
+    borderRadius: 2,
+    height: 30,
+    // padding: '0 10px',
+    marginTop: "5px",
+    padding: '0px 5px 0px 5px',
+  },
+  icon: {
+    padding: '0px 0px 0px 0px',
+    border: '0px 0px 0px 0px'
+  },
+  navDisplayFlex: {
+    display: `flex`,
+    justifyContent: `space-between`
+  },
+  linkText: {
+    textDecoration: `none`,
+    textTransform: `uppercase`,
+    color: `white`
+  },
+  drawer: {
+    width: "180px"
+  },
+  content: {
+    marginLeft: "240px"
+  }
+});
+
+function PositivityChart(props) {
+  const chart = useRef(null);
+  
+  //let [data, setData] = useState([]);
+  console.log('PositivityChart running...')
+  if(props && props.props && chart && chart.current) {
+    console.log('ok..');
+    let x = am4core.create("chartdiv", am4charts.XYChart);
+    x.data = props.props;
+    x.paddingRight = 40;
+
+    let dateAxis = x.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.grid.template.location = 0;
+
+    let valueAxis = x.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.tooltip.disabled = true;
+    valueAxis.renderer.minWidth = 35;
+
+    let series = x.series.push(new am4charts.LineSeries());
+    series.dataFields.dateX = "date";
+    series.dataFields.valueY = "value";
+
+    series.tooltipText = "{valueY.value}";
+    x.cursor = new am4charts.XYCursor();
+
+    let scrollbarX = new am4charts.XYChartScrollbar();
+    scrollbarX.series.push(series);
+    x.scrollbarX = scrollbarX;
+
+    chart.current = x;
+  }
+  //console.log(`props: ${JSON.stringify(props)}`);
+  
+  useEffect(() => {
+    let x = am4core.create("chartdiv", am4charts.XYChart);
+    
+    if(props && props.props ) {
+      x.data = props.props;
+      x.paddingRight = 40;
+
+      //console.log(`xData: ${x.data}`);
+      let dateAxis = x.xAxes.push(new am4charts.DateAxis());
+      dateAxis.renderer.grid.template.location = 0;
+
+      let valueAxis = x.yAxes.push(new am4charts.ValueAxis());
+      valueAxis.tooltip.disabled = true;
+      valueAxis.renderer.minWidth = 35;
+
+      let series = x.series.push(new am4charts.LineSeries());
+      series.dataFields.dateX = "date";
+      series.dataFields.valueY = "value";
+
+      series.tooltipText = "{valueY.value}";
+      x.cursor = new am4charts.XYCursor();
+
+      let scrollbarX = new am4charts.XYChartScrollbar();
+      scrollbarX.series.push(series);
+      x.scrollbarX = scrollbarX;
+
+      chart.current = x;
+    }
+    return () => {
+      chart.current && chart.current.dispose();
+      x.dispose();
+    };
+  }, []);
+
+  return (
+    <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
+  );
+}
+
+function DeathChart(props) {
+  const chart = useRef(null);
+  
+  //let [data, setData] = useState([]);
+  console.log('DeathChart running...')
+  if(props && props.props && chart && chart.current) {
+    console.log('ok..');
+    let x = am4core.create("chartdiv2", am4charts.XYChart);
+    x.data = props.props;
+    x.paddingRight = 40;
+
+    let dateAxis = x.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.grid.template.location = 0;
+
+    let valueAxis = x.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.tooltip.disabled = true;
+    valueAxis.renderer.minWidth = 35;
+
+    let series = x.series.push(new am4charts.LineSeries());
+    series.dataFields.dateX = "date";
+    series.dataFields.valueY = "value";
+
+    series.tooltipText = "{valueY.value}";
+    x.cursor = new am4charts.XYCursor();
+
+    let scrollbarX = new am4charts.XYChartScrollbar();
+    scrollbarX.series.push(series);
+    x.scrollbarX = scrollbarX;
+
+    chart.current = x;
+  }
+  //console.log(`props: ${JSON.stringify(props)}`);
+  
+  useEffect(() => {
+    let x = am4core.create("chartdiv2", am4charts.XYChart);
+    
+    if(props && props.props ) {
+      x.data = props.props;
+      x.paddingRight = 40;
+
+      //console.log(`xData: ${x.data}`);
+      let dateAxis = x.xAxes.push(new am4charts.DateAxis());
+      dateAxis.renderer.grid.template.location = 0;
+
+      let valueAxis = x.yAxes.push(new am4charts.ValueAxis());
+      valueAxis.tooltip.disabled = true;
+      valueAxis.renderer.minWidth = 35;
+
+      let series = x.series.push(new am4charts.LineSeries());
+      series.dataFields.dateX = "date";
+      series.dataFields.valueY = "value";
+
+      series.tooltipText = "{valueY.value}";
+      x.cursor = new am4charts.XYCursor();
+
+      let scrollbarX = new am4charts.XYChartScrollbar();
+      scrollbarX.series.push(series);
+      x.scrollbarX = scrollbarX;
+
+      chart.current = x;
+    }
+    return () => {
+      chart.current && chart.current.dispose();
+      x.dispose();
+    };
+  }, []);
+
+  return (
+    <div id="chartdiv2" style={{ width: "100%", height: "500px" }}></div>
+  );
+}
+
+function ICUChart(props) {
+  const chart = useRef(null);
+  
+  //let [data, setData] = useState([]);
+  console.log('DeathChart running...')
+  if(props && props.props && chart && chart.current) {
+    console.log('ok..');
+    let x = am4core.create("chartdiv3", am4charts.XYChart);
+    x.data = props.props;
+    x.paddingRight = 40;
+
+    let dateAxis = x.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.grid.template.location = 0;
+
+    let valueAxis = x.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.tooltip.disabled = true;
+    valueAxis.renderer.minWidth = 35;
+
+    let series = x.series.push(new am4charts.LineSeries());
+    series.dataFields.dateX = "date";
+    series.dataFields.valueY = "value";
+
+    series.tooltipText = "{valueY.value}";
+    x.cursor = new am4charts.XYCursor();
+
+    let scrollbarX = new am4charts.XYChartScrollbar();
+    scrollbarX.series.push(series);
+    x.scrollbarX = scrollbarX;
+
+    chart.current = x;
+  }
+  //console.log(`props: ${JSON.stringify(props)}`);
+  
+  useEffect(() => {
+    let x = am4core.create("chartdiv3", am4charts.XYChart);
+    
+    if(props && props.props ) {
+      x.data = props.props;
+      x.paddingRight = 40;
+
+      //console.log(`xData: ${x.data}`);
+      let dateAxis = x.xAxes.push(new am4charts.DateAxis());
+      dateAxis.renderer.grid.template.location = 0;
+
+      let valueAxis = x.yAxes.push(new am4charts.ValueAxis());
+      valueAxis.tooltip.disabled = true;
+      valueAxis.renderer.minWidth = 35;
+
+      let series = x.series.push(new am4charts.LineSeries());
+      series.dataFields.dateX = "date";
+      series.dataFields.valueY = "value";
+
+      series.tooltipText = "{valueY.value}";
+      x.cursor = new am4charts.XYCursor();
+
+      let scrollbarX = new am4charts.XYChartScrollbar();
+      scrollbarX.series.push(series);
+      x.scrollbarX = scrollbarX;
+
+      chart.current = x;
+    }
+    return () => {
+      chart.current && chart.current.dispose();
+      x.dispose();
+    };
+  }, []);
+
+  return (
+    <div id="chartdiv3" style={{ width: "100%", height: "500px" }}></div>
+  );
+}
+
+const renderICUChartData = (ICUChartData) => <div><h2>Currently in ICU:</h2><ICUChart props={ ICUChartData }/> </div>;
+
+function App() {
+  const classes = useStyles();
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "init":
+        return { ...state };
+      default:
+        return { ...state };
+    }
+  };
+  let [state, dispatch] = useReducer(reducer, { left: false, req: false, data: [], chartData: [], deathChartData: [], ICUChartData: [], notes: "", link: "Home", currentState: "" });
+
+  if(!state.req) {
+    state.req = true;
+    axios.get('https://api.covidtracking.com/v1/states/info.json')
+      .then(data => {
+        state.data = data.data;
+        //console.log(`axios: ${JSON.stringify(data.data, null, 2)}`);
+        dispatch(state, { type: "init" });
+      });
+  }
+  
+  const toggleDrawer = (anchor, open) => (event) => {
+    console.log(`EVENT: ${event}`);
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    dispatch(state);
+  };
+
+  {/* renderData will extract ignore the event, but use the USA - state - to make changes to the Base UI component */}
+  const renderData = (unitedState) => (event) => {
+    let chartData = [];
+    let deathChartData = [];
+    let ICUChartData = [];
+    state.chartData = [];
+    state.deathChartData = [];
+    state.ICUChartData = [];
+    //console.log(`onClick: ${usStates[unitedState]}`);
+    //let d = state.data;
+    state.link = unitedState;
+    let NotesObj = state.data.filter(e => e.state === usStates[unitedState])[0];
+    state.currentState = NotesObj.state;
+    //console.log(state);
+    state.notes = NotesObj.notes;
+    axios.get(`https://api.covidtracking.com/v1/states/${usStates[unitedState]}/daily.json`)
+      .then(USStateData => {
+        //console.log(JSON.stringify(USStateData,null,2));
+        
+        for(let i = 0; i < USStateData.data.length; i++) {
+          chartData.push({ date: USStateData.data[i].dateChecked, name: `name-${i}`, value: USStateData.data[i].positive })
+          deathChartData.push({ date: USStateData.data[i].dateChecked, name: `name-${i}`, value: USStateData.data[i].death })
+          ICUChartData.push({ date: USStateData.data[i].dateChecked, name: `name-${i}`, value: USStateData.data[i].inIcuCurrently })
+        }
+        //state.chartData = USStateData.data.map((e, i) => { return { date: e.dateChecked, name: `name-${i}`, value: e.positive }; })
+        state.chartData = chartData.filter(e => e.date !== null);
+        state.deathChartData = deathChartData.filter(e => e.date !== null);
+        state.ICUChartData = ICUChartData.filter(e => e.date !== null && e.value !== null);
+        console.log(JSON.stringify(state.ICUChartData,null,2));
+        //console.log(`${usStates[unitedState]}: ${JSON.stringify(USStateData,null,2)}`);
+        //console.log(`state: ${JSON.stringify(state.chartData,null,2)}`);
+        dispatch(state);
+      })
+    
+  }
+  const homePage = () => () => {
+    state.link = "Home";
+    dispatch(state);
+  }
+
+  const internationalPage = () => () => {
+    state.link = "International Cases";
+    dispatch(state);
+  }
+  
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list)}
+      role="presentation"
+    >
+      <List>
+        {['Home'].map((text) => (
+          <ListItem button dense key={text} onClick={homePage(text)} className={classes.root}>
+            <ListItemIcon className={classes.icon}>
+              <HomeIcon style={{ color: "blue", fontSize: "1rem" }}/>
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+
+
+      <List>
+        {['International Cases'].map((text) => (
+          <ListItem button dense key={text} onClick={internationalPage(text)} className={classes.root}>
+            <ListItemIcon className={classes.icon}>
+              <PublicIcon style={{ color: "blue", fontSize: "1rem" }}/>
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      
+      <Divider />
+      <List>
+
+        {/* This is the list of states on the left drawer */}
+          {Object.keys(usStates).map((text, index) => (
+            <ListItem button dense 
+                key={text} 
+                onClick={renderData(text)}
+                className={classes.root}
+            >
+              <ListItemIcon className={classes.icon}>
+                <LocationSearchingIcon style={{ color: "blue", fontSize: "1rem" }} />
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <React.Fragment key="left">
+              <MenuIcon onClick={toggleDrawer("left", true)} />
+              <div className={classes.drawer}>
+                <Drawer variant="permanent" anchor="left" open={state["left"]} onClose={toggleDrawer("left", false)}>
+                  {list("left")}
+                </Drawer>
+              </div>
+            </React.Fragment> 
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            <img src={image2vector} alt="image2vector" />
+            a COVID-19 tracker 
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+
+      {( (state.req)? console.log(`BLERP: ${state.currentState}`) : '' )}
+
+      <div className={classes.content}>
+        {/* { Object.keys(usStates).join(" ") } */}
+      </div>
+
+      {/* Render the home page until click dispatch */}
+      {
+        (state.link === "Home")?
+        <div className={classes.content}>
+
+          <h1> United States COVID-19 cases: </h1>
+          <iframe src="https://interactives.ap.org/embeds/Ow74t/26/" width="100%" height="700" frameborder="0"></iframe>
+          <iframe src="https://public.domo.com/cards/azrGr" width="100%" height="600" marginheight="0" marginwidth="0" frameborder="0"></iframe>
+          <iframe src="https://public.domo.com/cards/aAR5B" width="100%" height="700" marginheight="0" marginwidth="0" frameborder="0"></iframe>
+
+          <h2> 
+            Data is refreshed daily and collected from 
+            <a href = "https://coronavirus.jhu.edu/"> John Hopkins University</a>,
+            <a href = "https://covidtracking.com/"> The Covid Tracking Project</a>, 
+            as well as <a href = "https://www.domo.com/">DOMO</a>. 
+          </h2>       
+        </div>
+        :
+        (state.link === "International Cases")?
+        <div className={classes.content}>
+          <h1> International COVID-19 cases: </h1>
+          <div className={classes.counties}>
+            <iframe src="https://ourworldindata.org/grapher/total-cases-covid-19?tab=map" width="100%" height="600px" frameborder="0"></iframe>
+            <iframe src="https://public.domo.com/cards/dJ45D" width="100%" height="600" marginheight="0" marginwidth="0" frameborder="0"></iframe>
+            <iframe src="https://public.domo.com/cards/dwoBJ" width="100%" height="600" marginheight="0" marginwidth="0" frameborder="0"></iframe>
+          </div>
+          <h2>Data is refreshed daily and collected from 
+            <a href = "https://ourworldindata.org/"> Our World in Data</a>, 
+            as well as <a href = "https://www.domo.com/">DOMO</a>.</h2>
+        </div>
+        :
+        (state.currentState)?
+          <div className={ classes.content }>
+            <h1>
+            { state.link }
+            </h1>
+              <h2>
+                Positivity Rate:
+              </h2>
+              <PositivityChart props={ state.chartData }/>
+              <h2>
+                Death Rate:
+              </h2>
+              <DeathChart props={ state.deathChartData }/>
+              
+              {
+                (state.ICUChartData.length > 10)?
+                 renderICUChartData(state.ICUChartData)
+                :
+                  ""
+              }
+              
+            <h2>
+              Notes:
+            </h2>
+              <ReactMarkdown>{ state.notes }</ReactMarkdown>
+          </div>
+          :
+          ""
+      }
+      
+    </div>
+  );
+}
+
+
+export default App;
